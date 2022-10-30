45e6cd6cdba6902fdd39777e851e8f5a289c7de4
Susan <susan23codes@gmail.com>
2022-10-30 12:21:00 -0400

    had to take off navbar margin:

--- src/Navbar.js
+++ src/Navbar.js
@@ -41,24 +41,6 @@ export default function Navbar(props) {
     }
 
     return (
-        // <div className="navbar">
-        //     <h1 className="navbar-title">My Blood Pressure Tracker</h1>
-        //     {isLoggedIn &&
-        //         <div className="navbar-buttons">
-        //             <div className='results-buttons'>
-        //                 {/* <span style={{ color: "white", textDecoration: "underline" }}>Results:</span> */}
-        //                 <Link to="/resultslist" style={{ color:"white", textDecoration: "none" }} className="results-list">All Readings</Link>
-        //                 <Link to="/results" style={{ color:"white", textDecoration: "none" }} className="results-list">Data Visualization</Link>
-
-        //             </div>
-        //             <div className='home-logout-buttons'>
-        //                 <Link to="/" style={{ color: "white", textDecoration: "none" }}>Home</Link>
-        //                 <span> &ensp; | &ensp; </span>
-        //                 <Link to="/" onClick={handleLogout} style={{ color: "white", textDecoration: "none" }}>Logout</Link>
-        //             </div>
-        //         </div>
-        //     }
-        // </div >
 
         <div class="navbar">
             <div class="container">
